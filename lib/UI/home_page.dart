a5c91d673eb0ed9e428161273819c39a9212a6b9
Amit <amit.mishra@neosofttech.com>
2021-10-20 19:59:20 +0530

    HomePage code refactor

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -6,8 +6,7 @@ import '../controller/bmi_controller.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int height = 0, weight = 0;
-    final BMIController bmi = Get.put(BMIController());
+    final BMIController bmiController = Get.put(BMIController());
 
     return Scaffold(
       appBar: AppBar(title: Text('getxdemo')),
@@ -15,45 +14,79 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 32),
-            const Text('BMI',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            Obx(() => Text(bmi.bmiValue.value.toStringAsFixed(2),
-                style: const TextStyle(
-                    fontWeight: FontWeight.bold, fontSize: 28))),
+            _bmiText(),
+            _bmiValue(bmiController),
             const SizedBox(height: 32),
-            const Text(
-              'Enter your weight in Kg:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: bmi.weightTextController,
-                decoration: InputDecoration(
-                    border: const OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue))),
-              ),
-            ),
-            const Text(
-              'Enter your height:',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: bmi.heightTextController,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.blue))),
-              ),
-            ),
-            ElevatedButton(
-                onPressed: () => bmi.getBMI(), child: const Text('Get BMI')),
-            ElevatedButton(
-                onPressed: () => Get.toNamed('/result'), child: const Text('More Info')),
+            _enterWeightText(),
+            _weightInput(bmiController),
+            _enterHeightText(),
+            _heightInput(bmiController),
+            _getBMIButton(bmiController),
+            _moreInfoButton(),
           ],
         ),
       ),
     );
+  }
+
+  ElevatedButton _moreInfoButton() {
+    return ElevatedButton(
+              onPressed: () => Get.toNamed('/result'),
+              child: const Text('More Info'));
+  }
+
+  ElevatedButton _getBMIButton(BMIController bmiController) {
+    return ElevatedButton(
+              onPressed: () => bmiController.getBMI(),
+              child: const Text('Get BMI'));
+  }
+
+  Padding _heightInput(BMIController bmiController) {
+    return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: bmiController.heightTextController,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue))),
+            ),
+          );
+  }
+
+  Text _enterHeightText() {
+    return const Text(
+            'Enter your height in M:',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          );
+  }
+
+  Padding _weightInput(BMIController bmiController) {
+    return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: bmiController.weightTextController,
+              decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue))),
+            ),
+          );
+  }
+
+  Text _enterWeightText() {
+    return const Text(
+            'Enter your weight in Kg:',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          );
+  }
+
+  Obx _bmiValue(BMIController bmiController) {
+    return Obx(() => Text(bmiController.bmiValue.value.toStringAsFixed(2),
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 28)));
+  }
+
+  Text _bmiText() {
+    return const Text('BMI',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20));
   }
 }
